23d18b30c658ca0f953b4e661a101b8ad7677cbf
Machimass <wss.raynertwc@gmail.com>
2026-02-04 18:28:37 +0800

    Cutscene

---
+++ kOS/ScriptingDLL/Scripts/Include/CutSceneScript.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "ECS/Component/Component.h"
+#include "Config/pch.h"
+#include "ScriptAdapter/TemplateSC.h"
+
+
+class CutSceneScript : public TemplateSC {
+public:
+
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+
+	REFLECTABLE(CutSceneScript);
+};
