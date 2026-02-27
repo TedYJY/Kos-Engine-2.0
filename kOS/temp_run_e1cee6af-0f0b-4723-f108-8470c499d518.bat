f386198f887c71ad466e7b79c343f5f3b05a0dd1
raymondyeosk <raymondyeosk@gmail.com>
2026-02-27 16:25:28 +0800

    sandbox

---
+++ kOS/temp_run_e1cee6af-0f0b-4723-f108-8470c499d518.bat
@@ -0,0 +1,3 @@
+@echo off
+set EXE="C:\Users\RAYMOND\Desktop\Group Projects\GAM GITHUB\kOS\Kos Editor\Compilers\Executable\Mesh Compiler\MeshCompiler.exe"
+%EXE% "C:\Users\RAYMOND\Desktop\Group Projects\GAM GITHUB\kOS\Kos Editor\Assets\Models\M3 Assets\M3Level.fbx" "C:\Users\RAYMOND\Desktop\Group Projects\GAM GITHUB\kOS\Kos Editor\Assets\Models\M3 Assets\M3Level.fbx.meta" "C:\Users\RAYMOND\Desktop\Group Projects\GAM GITHUB\kOS\Resource\e1cee6af-0f0b-4723-f108-8470c499d518.mesh" ""
